30b90ce37ef69501bc7cb5de3598526d67ce46cf
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2022-07-26 14:56:12 +0300

    Fix `embroider-optimized` scenario

--- addon/components/models-table/themes/ember-bootstrap-v4/floating-filter.ts
+++ addon/components/models-table/themes/ember-bootstrap-v4/floating-filter.ts
@@ -1,3 +1,19 @@
+import { ensureSafeComponent } from '@embroider/util';
 import ModelsTableFloatingFilterComponent from '../default/floating-filter';
+import {
+  getBsDropdown,
+  getBsButton,
+  getBsForm,
+} from '../../../../utils/emt/themes/ebs';
 
-export default class FloatingFilter extends ModelsTableFloatingFilterComponent {}
+export default class FloatingFilter extends ModelsTableFloatingFilterComponent {
+  get BsDropdown(): unknown {
+    return ensureSafeComponent(getBsDropdown(), this);
+  }
+  get BsButton(): unknown {
+    return ensureSafeComponent(getBsButton(), this);
+  }
+  get BsForm(): unknown {
+    return ensureSafeComponent(getBsForm(), this);
+  }
+}
